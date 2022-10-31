2930acfde7dcba03762d0e30b3c735e2d78a12e3
Santiago Musso <santiagomusso@gmail.com>
2022-10-31 13:29:00 -0300

    aplicando variables de entorno

--- react/clase-1/src/services/firebaseConfig.js
+++ react/clase-1/src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCu4nIQM8QW9U6Q8AttE_0y6L-zePOPmqA',
-  authDomain: 'react-trapani.firebaseapp.com',
-  projectId: 'react-trapani',
-  storageBucket: 'react-trapani.appspot.com',
-  messagingSenderId: '544061750048',
-  appId: '1:544061750048:web:1d56b1b96836278a5b3545'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
